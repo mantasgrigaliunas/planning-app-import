174c995abccbb557d34a378a7fcdb786966cea6d
Mantas Grigaliunas <mantas.grigaliunas@arcusglobal.com>
2015-03-17 09:45:15 +0000

    fix

--- web/index.php
+++ web/index.php
@@ -3,13 +3,13 @@
 // Variables used globally - will be set from URL (see above code)
 $planningPortalLPA = 'M3645';
 $planningPortalLPAPassword = 'ta8ndri3dge';
-$salesforceUsername = "X";
-$salesforceToken = "X";
-$salesforcePassword = "X";
+$salesforceUsername = "mantas.grigaliunas@arcus.built.mantas";
+$salesforceToken = "9rktHA2u0NWJX6Jh4PTh6lbX";
+$salesforcePassword = "#arcus123";
 
 ini_set('max_execution_time', 300);  //300 seconds = 5 minutes
 //  Requires
-require_once 'inc/PlanningPortalConnector.php';
+//require_once 'inc/PlanningPortalConnector.php';
 require_once 'inc/SalesForceConnector.php';
 require_once 'inc/LogEntriesAPI/logentries.php';
 require_once 'inc/functions.php';
@@ -17,10 +17,10 @@ require_once 'inc/functions.php';
 // MAIN ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  If we have some stuff in $_GET then let begin..
 
-    $PP = new PlanningPortalConnection($planningPortalLPA, $planningPortalLPAPassword, $log);
+    //$PP = new PlanningPortalConnection($planningPortalLPA, $planningPortalLPAPassword, $log);
     $SF = new SalesforceConnection($salesforceUsername, $salesforcePassword, $salesforceToken, $log);
 
-    $log->Debug("DOWNLOAD STARTED FOR " . $planningPortalLPA);
+    print_r("DOWNLOAD STARTED FOR " . $planningPortalLPA);
 
 //  For each application on Planning Portal
     try {
