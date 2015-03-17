4f0c0ac8e52ffb37aa3c6f5c937a0ad69555cb01
Mantas Grigaliunas <mantas.grigaliunas@arcusglobal.com>
2015-03-17 09:53:46 +0000

    fix

--- web/index.php
+++ web/index.php
@@ -20,45 +20,7 @@ require_once 'inc/functions.php';
     //$PP = new PlanningPortalConnection($planningPortalLPA, $planningPortalLPAPassword, $log);
     $SF = new SalesforceConnection($salesforceUsername, $salesforcePassword, $salesforceToken, $log);
 
-    print_r("DOWNLOAD STARTED FOR " . $planningPortalLPA);
-
-//  For each application on Planning Portal
-    try {
-        foreach ($PP->GetListOfProposals() as $PlanningPortalApplication) {
-
-            $log->Info("Planning Portal Application Reference: " . $PlanningPortalApplication->RefNum);
-
-            //  Get the whole Proposal
-            $fullApplication = $PP->GetFullProposal($PlanningPortalApplication->RefNum);
-
-
-            //  Get the main XML body with all proposal info (Whole Proposal Less Attachments)
-            $applicationInformation = $PP->GetPlanningApplicationInformation($fullApplication);
-
-
-            // Upload Attachement information to SF (returns SF Object ID)
-            $SFPlanningApplication = $SF->CreatePlanningApplication($applicationInformation);
-
-            if ($SFPlanningApplication != "ERROR") {
-                //  Get an array of attachments
-
-                $attachmentArray = $PP->GetAttachments($fullApplication);
-                foreach ($attachmentArray as $attachment) {
-                    // Split the attachment body and content
-                    list($rawheaders, $body) = preg_split("/\R\R/", $attachment, 2);
-                    // Get the Attahcement Headers
-                    $headers = $PP->GetAttachmentHeaders($rawheaders);
-                    // Upload the attachment!
-                    $attachtmentId = $SF->CreateAttachment($body, $headers['File Name'], $SFPlanningApplication);
-                }
-                $planningFee = $SF->CreateFee($SFPlanningApplication, $applicationInformation->Body->Proposal->ApplicationHeader->Payment->AmountDue);
-            }
-        }
-    } catch (Exception $e) {
-      print_r($e);
-        $log->Error("ERROR! Caught Exception: " . $e->getMessage());
-    }
-    $log->Debug("DOWNLOAD COMPLETED FOR " . $planningPortalLPA);
+    print_r('Connected');
     
 echo "Process Completed";
 
