10dcdf8d3be137ce6fa9463bd91aabe5b3234335
Mantas Grigaliunas <mantas.grigaliunas@arcusglobal.com>
2015-03-20 10:37:37 +0000

    fix

--- web/inc/SalesForceConnector.php
+++ web/inc/SalesForceConnector.php
@@ -471,9 +471,9 @@ class SalesforceConnection
     public function SFResponceMessage($SFResponce)
     {
         if($SFResponce[0]->success == 1){
-            print_r("Success : " . implode($SFResponce) . "<br><br>");
+            print_r("Success : " . implode(" ", $SFResponce) . "<br><br>");
         } else {
-            print_r("Failure : " . implode($SFResponce) . "<br><br>");
+            print_r("Failure : " . implode(" ", $SFResponce) . "<br><br>");
         }
     }
 
